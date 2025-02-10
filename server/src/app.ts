1fbdcf3216c4be596f2a79d6fd99a46601ef7c50
kasper666g <64731263+kasper666g@users.noreply.github.com>
2025-02-10 18:34:39 +0200

    first commit

---
+++ server/src/app.ts
@@ -0,0 +1,12 @@
+import express from 'express';
+import cors from 'cors';
+const app = express();
+const PORT = 1234;
+app.use(cors({ origin: 'http://localhost:3000' }));
+app.use(express.json());
+app.get('/api/test', (req, res) => {
+  res.json({ message: 'Moikka!' });
+});
+app.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
+});
