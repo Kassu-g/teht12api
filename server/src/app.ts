6e9a232ea4c35234e8e79e2c07167bd8302186b6
kasper666g <64731263+kasper666g@users.noreply.github.com>
2025-02-10 19:10:21 +0200

    dsad

--- server/src/app.ts
+++ server/src/app.ts
@@ -1,12 +1,29 @@
 import express from 'express';
+import mongoose from 'mongoose';
 import cors from 'cors';
 const app = express();
 const PORT = 1234;
-app.use(cors({ origin: 'http://localhost:3000', optionsSuccessStatus: 200, }));
+app.use(cors());
 app.use(express.json());
-app.get('/api/test', (req, res) => {
-  res.json({ message: 'Moikka!' });
+mongoose.connect('mongodb://localhost:27017/booksdb', {
+}).then(() => console.log('MongoDB connected'))
+  .catch(err => console.error(err));
+
+const kirja = new mongoose.Schema({
+  author: String,
+  name: String,
+  pages: Number,
+});
+const Book = mongoose.model('Book', kirja);
+app.post('/api/book', async (req, res) => {
+  try {
+    const uusi = new Book(req.body);
+    await uusi.save();
+    res.status(201).json(uusi);
+  } catch (error) {
+    res.status(500).json({ message: 'virhe', error });
+  }
 });
 app.listen(PORT, () => {
-  console.log(`Server is running on http://localhost:${PORT}`);
+  console.log(`Täällä toimii http://localhost:${PORT}`);
 });
