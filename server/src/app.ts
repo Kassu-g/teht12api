ff8c0c7ee61447ceff0ae40176c5207bb89acdee
kasper666g <64731263+kasper666g@users.noreply.github.com>
2025-02-10 18:45:51 +0200

    sssss

--- server/src/app.ts
+++ server/src/app.ts
@@ -2,7 +2,7 @@ import express from 'express';
 import cors from 'cors';
 const app = express();
 const PORT = 1234;
-app.use(cors({ origin: 'http://localhost:3000' }));
+app.use(cors({ origin: 'http://localhost:3000', optionsSuccessStatus: 200, }));
 app.use(express.json());
 app.get('/api/test', (req, res) => {
   res.json({ message: 'Moikka!' });
